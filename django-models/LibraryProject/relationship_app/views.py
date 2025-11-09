22773b80a139967eeec1172cdbfd1577f87be71f
HajarOuq <hajar.ouqacem@gmail.com>
2025-11-09 21:59:19 +0100

    django models 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,6 @@ from django.views.generic.detail import DetailView
 from django.views.generic import DetailView
 from .models import Book
 from .models import Library
-from .views import list_books
 
 
 # ------------------------------
