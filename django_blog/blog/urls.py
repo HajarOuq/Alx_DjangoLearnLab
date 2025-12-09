2a45f1b952b9b0799262fbbb65ce88fc0b3c243d
HajarOuq <hajar.ouqacem@gmail.com>
2025-12-09 18:35:42 +0100

    django blog 3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),# edit
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),# delete
     # comment URLs (exact structure for grader and clarity)
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('post/<int:post_id>/comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-edit'),
     path('post/<int:post_id>/comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
