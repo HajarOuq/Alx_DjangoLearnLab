b25c304ca6faa8f5914edd545aacca8c7dbf2e69
HajarOuq <hajar.ouqacem@gmail.com>
2025-12-14 15:43:59 +0100

    social_media_api 2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -3,9 +3,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from django.contrib.auth import authenticate
-
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import RegisterSerializer
+
+User = get_user_model()
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -43,5 +48,28 @@ class ProfileView(APIView):
             'email': user.email,
             'bio': user.bio
         })
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    request.user.following.add(user_to_follow)
+    return Response({'message': 'User followed'}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    request.user.following.remove(user_to_unfollow)
+    return Response({'message': 'User unfollowed'}, status=status.HTTP_200_OK)
 
 
