f557338b54091f97ff37dc254c6ea2491b84b8b6
HajarOuq <hajar.ouqacem@gmail.com>
2025-12-14 13:47:28 +0100

    social_media_api 0

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import authenticate
+
+from .serializers import RegisterSerializer
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key})
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get('username'),
+            password=request.data.get('password')
+        )
+        if not user:
+            return Response({'error': 'Invalid credentials'}, status=400)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'username': user.username,
+            'email': user.email,
+            'bio': user.bio
+        })
+
+
