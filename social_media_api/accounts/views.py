0bed4935958f2f1fc5bdf75891fa128eb7a96fbc
HajarOuq <hajar.ouqacem@gmail.com>
2025-12-14 15:47:35 +0100

    social_media_api 2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,6 +9,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -73,3 +77,23 @@ def unfollow_user(request, user_id):
     return Response({'message': 'User unfollowed'}, status=status.HTTP_200_OK)
 
 
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        users = CustomUser.objects.all()
+        user_to_follow = get_object_or_404(users, id=user_id)
+
+        request.user.following.add(user_to_follow)
+        return Response({"detail": "User followed"}, status=status.HTTP_200_OK)
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        users = CustomUser.objects.all()
+        user_to_unfollow = get_object_or_404(users, id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+        return Response({"detail": "User unfollowed"}, status=status.HTTP_200_OK)
